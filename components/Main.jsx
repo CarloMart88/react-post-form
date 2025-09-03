edad460ed477387cc0ce1699ac08ea087d8d5cc0
CarloMart88 <carlo.martino1988@gmail.com>
2025-09-03 16:23:08 +0200

    feat: logic handleChange

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+//importo useState e axios
+import { useState } from "react";
+import axios from "axios";
+
+// link
+const link = "https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts";
 
 function Main() {
+  // creo l'oggetto con dentro i valori delle proprietà
+  const initialFormAuthors = {
+    name: "",
+    author: "",
+    title: "",
+    body: "",
+    public: false,
+  };
+
+  // la variabilie di stato
+  const [formAuthors, setformAuthors] = useState(initialFormAuthors);
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    // creo il nuovo oggetto una volta destrutturato e.target
+    const newObj = {
+      ...formAuthors,
+      [name]: type === "checkbox" ? checked : value,
+    };
+    //uso il nuovo oggetto
+    setformAuthors(newObj);
+  };
+
   return (
     <div className="container m-5">
       <div className="row">
