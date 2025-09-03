4ef7eec54b028a65ba6ded67d79b770a4b46cd51
CarloMart88 <carlo.martino1988@gmail.com>
2025-09-03 15:53:45 +0200

    feat: HTML structure ready

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,8 +2,42 @@ import React from "react";
 
 function Main() {
   return (
-    <div>
-      <h1>iniziamo</h1>
+    <div className="container m-5">
+      <div className="row">
+        <form>
+          <div className="mb-3 col-5">
+            <div className="">Autore</div>
+            <input type="text" className="form-control" />
+            <div id="" className="form-text">
+              Per piacere riempire il campo indicato
+            </div>
+          </div>
+          <div className="mb-3 col-5">
+            <div className="">Titolo</div>
+            <input type="text" className="form-control" />
+            <div id="" className="form-text">
+              Per piacere riempire il campo indicato
+            </div>
+          </div>
+          <div className="mb-5 col-5">
+            <div className="">Articolo</div>
+            <input type="text" className="form-control" />
+            <div className="form-text">
+              Per piacere riempire il campo indicato
+            </div>
+          </div>
+          <hr />
+          <div className="mb-5 form-check">
+            <input type="checkbox" className="form-check-input" />
+            <div className="form-check-label">Pubblica</div>
+            <input type="checkbox" className="form-check-input" />
+            <div className="form-check-label">Bozza</div>
+          </div>
+          <button type="submit" className="btn btn-success">
+            Invia
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
